c94b684bc8309b002d7deac653945fbcfe064ab3
Файрушин Равил <r.fajrushin@dssl.ru>
2024-04-03 10:49:19 +0300

    add filename

--- lua/community.lua
+++ lua/community.lua
@@ -11,7 +11,8 @@ return {
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.colorscheme.nightfox-nvim" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
-  { import = "astrocommunity/recipes/heirline-nvchad-statusline" },
+  -- { import = "astrocommunity/recipes/heirline-nvchad-statusline" },
+  -- { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
 
   -- import/override with your plugins folder
 }
